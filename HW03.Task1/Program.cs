256a0a94e90b0703a82796e24e3dbe57aacf54cd
DmitriiBachilo <dimamn7@gmail.com>
2020-03-08 17:30:55 +0300

    Add explicit and implicit conversion, boxing and unboxing tasks.

---
+++ HW03.Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HW03.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intValue = 1;
+            double doubleValue = 123.456;
+            decimal decimalValue = 165601651605656;
+            sbyte sbyteValue = 127;
+            float floatValue = 5.7979f;
+            char charValue = 'Z';
+
+            // a (explicit conversion)
+            char expConv1 = (char)intValue;
+            int expConv2 = (int)doubleValue;
+            long expConv3 = (long)decimalValue;
+
+            // b (implicit conversion)
+            short impConv1 = sbyteValue;
+            double impConv2 = floatValue;
+            long impConv3 = intValue;
+
+            // c (boxing)
+            object obj1 = intValue;
+            object obj2 = doubleValue;
+            object obj3 = floatValue;
+            
+            // d (unboxing)
+            int intForUnboxing = (int)obj1;
+            double doubleForUnboxing = (double)obj2;
+            float floatForUnboxing = (float)obj3;
+
+            // three examples
+            obj1 = intValue;
+            ushort ushortForUnboxing = (ushort)obj1;
+
+            obj2 = true;
+            byte byteForUnboxing = (byte)obj2;
+
+            obj3 = charValue;
+            intForUnboxing = (int)obj3;
+        }
+    }
+}
