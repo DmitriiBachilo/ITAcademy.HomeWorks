108a42c489f804e7697fc77b42660d5177751e25
DmitriiBachilo <dimamn7@gmail.com>
2020-03-29 19:09:42 +0300

    Add task 1

---
+++ HW_08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW08_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text: ");
+            string text = Console.ReadLine();
+            string[] strings = text.Split(';');
+            Console.WriteLine();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                strings[i] = strings[i].Replace("O", "A").Trim();
+            }
+
+            foreach (var str in strings)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
